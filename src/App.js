33abc273fef9b7d4fb22bd8c335d2d26f8a785cb
Eren Tasci <erent4sci@gmail.com>
2022-10-17 21:15:10 +0300

    added new features

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
-  console.log(feedback);
+  const deleteFeedBack = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  const addFeedBack = (newFeedBack) => {
+    newFeedBack.id = uuidv4();
+    console.log(typeof newFeedBack.id);
+    setFeedback([newFeedBack, ...feedback]);
+  };
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackForm handleAdd={addFeedBack} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedBack} />
       </div>
     </>
   );
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,13 +1,48 @@
 import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
+import { AnimatePresence, motion } from "framer-motion";
 
-function FeedbackList({ feedback }) {
-  return (
+function FeedbackList({ feedback, handleDelete }) {
+  {
+    /*return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
       ))}
+    </div>
+      ); */
+  }
+
+  return (
+    <div className="feedback-list">
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }} // Fade out effect with framer-motion
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
