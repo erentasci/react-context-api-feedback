47ff0e6e29787c3da8d3e47c3f94590fef386fa3
Eren Tasci <erent4sci@gmail.com>
2022-10-20 17:08:38 +0300

    Added json-server for backend and This project finished

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,11 +3,18 @@ import FeedbackItem from "./FeedbackItem";
 import { AnimatePresence, motion } from "framer-motion";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-  return (
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>;
+  }
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,28 +1,61 @@
-import { createContext, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import FeedbackData from "../data/FeedbackData";
+import { createContext, useState, useEffect } from "react";
+// import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvier = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
 
+  useEffect(() => {
+    fetchFeedback();
+  }, [feedback]);
+
+  // Fetch feedback
+
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
+
   // Delete feedback
-  const deleteFeedBack = (id) => {
+  // const deleteFeedBack = (id) => {
+  //   if (window.confirm("Are you sure you want to delete?")) {
+  //     setFeedback(feedback.filter((item) => item.id !== id));
+  //   }
+  // };
+
+  const deleteFeedBack = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   // Add feedback
-  const addFeedBack = (newFeedBack) => {
-    newFeedBack.id = uuidv4();
-    console.log(typeof newFeedBack.id);
-    setFeedback([newFeedBack, ...feedback]);
+  // const addFeedBack = (newFeedBack) => {
+  //   newFeedBack.id = uuidv4();
+  //   console.log(typeof newFeedBack.id);
+  //   setFeedback([newFeedBack, ...feedback]);
+  // };
+
+  const addFeedBack = async (newFeedBack) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFeedBack),
+    });
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   // Set Item to be Updated
@@ -33,9 +66,25 @@ export const FeedbackProvier = ({ children }) => {
     });
   };
 
-  const updateFeedback = (id, updItem) => {
+  // const updateFeedback = (id, updItem) => {
+  //   setFeedback(
+  //     feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+  //   );
+  // };
+
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = response.json();
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
@@ -44,6 +93,7 @@ export const FeedbackProvier = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedBack,
         addFeedBack,
         editFeedback,
